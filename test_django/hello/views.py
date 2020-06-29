eda6c0213aa6adcdf22bd677639188bf712447b3
ggamsi0418 <ggamsi0418@naver.com>
2020-06-29 19:12:59 +0900

    Docker-Jenkins 배포 자동화 테스트 - 07

--- test_django/hello/views.py
+++ test_django/hello/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Docker-Jenkins 배포 자동화 테스트 - 06")
+	return HttpResponse("Docker-Jenkins 배포 자동화 테스트 - 07")
